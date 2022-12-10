5a86735d29b85417b71d975eb48adc9e969a209d
Satyam <bajpai1973satyam@gmail.com>
2022-12-10 20:57:31 +0530

    added new route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8001;
+
+
+// app.use(express.json(), {extended: false})
 app.get("/", (req,res)=>{
     res.json({
         "status" : 200,
         "message": "API is running succesfully!"
     });
-    res.status(200).send();
+})
+
+app.get("/api/hello", (req,res)=>{
+    res.json({
+        "status" : 200,
+        "message": "Hello from the API!"
+    });
 })
 
 app.listen(PORT,()=>{
